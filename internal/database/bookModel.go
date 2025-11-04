54e3f1cec16bd251744edff36bb39aad85c9b3af
Nikitas Nikandros <Nikitas.NIKANDROS@ext.ec.europa.eu>
2025-11-04 09:31:18 +0000

    add json tags

--- internal/database/bookModel.go
+++ internal/database/bookModel.go
@@ -3,11 +3,11 @@ package database
 import "time"
 
 type BookModel struct {
-	Title           string
-	Author          string
-	PublicationDate time.Time
-	FinishedDate    time.Time
-	Rating          string
+	Title           string    `json:"title"`
+	Author          string    `json:"author"`
+	PublicationDate time.Time `json:"publication-date"`
+	FinishedDate    time.Time `json:"finished-date"`
+	Rating          string    `json:"rating"`
 }
 
 func (u BookModel) ToAddBookParams() AddBookParams {
